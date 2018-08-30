bbd1c3fe84911f8d3e40fbbb7178683229fec31b
kazuyaseki <track613armory@gmail.com>
2018-08-31 08:01:53 +0900

    Added redux-thunk

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,17 +2,20 @@ import { Store, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { routerMiddleware } from 'react-router-redux';
 import { History } from 'history';
+import thunk from 'redux-thunk';
 import { logger } from 'app/middleware';
 import { RootState, rootReducer } from 'app/reducers';
 
 export function configureStore(history: History, initialState?: RootState): Store<RootState> {
-  let middleware = applyMiddleware(logger, routerMiddleware(history));
+  let middleware = applyMiddleware(logger, routerMiddleware(history), thunk);
 
   if (process.env.NODE_ENV !== 'production') {
     middleware = composeWithDevTools(middleware);
   }
 
-  const store = createStore(rootReducer as any, initialState as any, middleware) as Store<RootState>;
+  const store = createStore(rootReducer as any, initialState as any, middleware) as Store<
+    RootState
+  >;
 
   if (module.hot) {
     module.hot.accept('app/reducers', () => {
