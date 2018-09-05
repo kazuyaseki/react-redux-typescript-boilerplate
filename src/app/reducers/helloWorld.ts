545e52308fd816f22e4065c4f85532fa56ab7baa
kazuyaseki <track613armory@gmail.com>
2018-09-05 22:40:53 +0900

    reducerも

--- src/app/reducers/helloWorld.ts
+++ src/app/reducers/helloWorld.ts
@@ -1,16 +1,30 @@
-import { ActionType } from 'app/actions/helloWorld';
+import { Action } from 'redux';
+import { isType } from 'typescript-fsa';
+import { ActionType, helloWorldActions } from 'app/actions/helloWorld';
 
-export const helloWorldReducer = (
-  state: string = '',
-  action: { type: string; payload: string }
-) => {
-  switch (action.type) {
-    case ActionType.success:
-      return action.payload;
-    case ActionType.failure:
-      return action.payload;
+// export const helloWorldReducer = (
+//   state: string = '',
+//   action: { type: string; payload: string }
+// ) => {
+//   switch (action.type) {
+//     case ActionType.success:
+//       return action.payload;
+//     case ActionType.failure:
+//       return action.payload;
 
-    default:
-      return state;
+//     default:
+//       return state;
+//   }
+// };
+
+export const helloWorldReducer = (state: string = '', action: Action) => {
+  if (isType(action, helloWorldActions.started)) {
+    return { bar: action.payload.foo };
   }
+
+  if (isType(action, helloWorldActions.done)) {
+    return { bar: action.payload.result.bar };
+  }
+
+  return state;
 };
